ecae8c102dae6329a4514c80844b7aa010ddb1eb
Luzian Serafin <luzianserafin@gmail.com>
2023-08-20 20:24:12 +0000

    Updated starter template

--- src/game.ts
+++ src/game.ts
@@ -1,65 +1,23 @@
-import { Engine, Vector, Input, Color, vec } from 'excalibur';
-import { Paddle } from './paddle';
-import { Ball } from './ball';
-import { Brick } from './brick';
+import { Engine, Color, vec, Label, Font } from 'excalibur';
 
 export class GameMain extends Engine {
-  private paddle: Paddle;
-  private ball: Ball;
-  private bricks: Brick[] = [];
   
   constructor() {
     super({ width: 800, height: 600 });
 
-    this.paddle = new Paddle(this);
-    this.add(this.paddle);
-
-    const ballSpeed = new Vector(100, 100);
-    this.ball = new Ball(this, ballSpeed);
-    this.add(this.ball);
-    
-    this.setupBricks();
-    this.setupInput();
-  }
-
-  private setupBricks(): void {
-    // Padding between bricks
-    const padding = 20; // px
-    const xoffset = 65; // x-offset
-    const yoffset = 20; // y-offset
-    const columns = 5;
-    const rows = 3;
-    const brickColor = [Color.Violet, Color.Orange, Color.Yellow];
-    
-    // Individual brick width with padding factored in
-    const brickWidth = this.drawWidth / columns - padding - padding / columns; // px
-    const brickHeight = 30; // px
-    
-    for (let j = 0; j < rows; j++) {
-      for (let i = 0; i < columns; i++) {
-        const brick = new Brick(
-          xoffset + i * (brickWidth + padding) + padding,
-          yoffset + j * (brickHeight + padding) + padding,
-          brickWidth,
-          brickHeight,
-          brickColor[j % brickColor.length]
-        );
-        
-        this.bricks.push(brick);
-        this.add(brick);
-      }
-    }
-}
-
-
-  private setupInput(): void {
-    this.input.pointers.primary.on('move', (evt: Input.PointerEvent) => {
-      this.paddle.moveTo(evt.worldPos.x);
+     // Create a new Label
+     const label = new Label({
+      text: `Hello World!`,
+      color: Color.Red,
+      pos: vec(300, 300),
+      font: new Font({ size: 30 })
     });
+
+    // Add the Label to the current scene
+    this.add(label);
   }
 
   public startGame(): void {
-    this.ball.serve(vec(100, 100));
     super.start();
   }
 }
